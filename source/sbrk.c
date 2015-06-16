30a581bc057180263f46194de567cb3680990d28
Brendan Moran <brendan.moran@arm.com>
2015-06-16 14:50:54 +0100

    Rework sbrk/krbs to not use nested exclusive ld/st pairs Add gitignore Add a preliminary test

--- source/sbrk.c
+++ source/sbrk.c
@@ -1,9 +1,14 @@
 #include <stdint.h>
 #include <stddef.h>
+#include "cmsis.h"
 
 #ifndef SBRK_ALIGN
 #define SBRK_ALIGN 4U
 #endif
+#if (SBRK_ALIGN & (SBRK_ALIGN-1))
+#error SBRK_ALIGN must be a power of 2
+#endif
+
 
 #ifndef SBRK_INC_MIN
 #define SBRK_INC_MIN (SBRK_ALIGN)
@@ -12,80 +17,85 @@
 #ifndef KRBS_ALIGN
 #define KRBS_ALIGN 4U
 #endif
+#if (KRBS_ALIGN & (KRBS_ALIGN-1))
+#error KRBS_ALIGN must be a power of 2
+#endif
 
 #ifndef KRBS_INC_MIN
 #define KRBS_INC_MIN (KRBS_ALIGN)
 #endif
 
-extern const void * KRBS_START;
-extern const void * SBRK_START;
+extern uint32_t KRBS_START;
+extern uint32_t SBRK_START;
 
-volatile void * krbs_ptr = KRBS_START;
-volatile void * sbrk_ptr = SBRK_START;
+volatile void * krbs_ptr = &KRBS_START;
+volatile void * sbrk_ptr = &SBRK_START;
+volatile uintptr_t sbrk_diff = 4096U;
 
 void * sbrk(const size_t size) {
-    uintptr_t sbrk_tmp;
-    size_t size_internal = size
+    uintptr_t sbrk_tmp = NULL;
+    size_t size_internal = size;
     // Guarantee minimum allocation size
     if (size_internal < SBRK_INC_MIN) {
-        size_internal = SBRK_INC_MIN
+        size_internal = SBRK_INC_MIN;
+    }
+    size_internal = ( size_internal + SBRK_ALIGN - 1) & ~(SBRK_ALIGN - 1);
+
+    while(1) {
+        uintptr_t ptr_diff = __LDREXW((uint32_t *)&sbrk_diff);
+        if (size_internal > ptr_diff) {
+            return NULL;
+        }
+        ptr_diff -= size_internal;
+        if (__STREXW(ptr_diff, (uint32_t *)&sbrk_diff))
+            continue;
+        break;
     }
 
-    do {
-        sbrk_tmp = __LDREXW(&sbrk_ptr);
-        const uintptr_t krbs_tmp = __LDREXW(&krbs_ptr);
+    while (1) {
+        sbrk_tmp = __LDREXW((uint32_t *)&sbrk_ptr);
         // store the base pointer for the allocated memory
         const uintptr_t sbrk_old = sbrk_tmp;
         // Calculate the new krbs pointer
         sbrk_tmp += size_internal;
         // Guarantee krbs alignment alignment
-        sbrk_tmp = ( sbrk_tmp + SBRK_ALIGN - 1) & ~(SBRK_ALIGN - 1)
-
-        if (krbs_tmp < sbrk_tmp) {
-            sbrk_tmp = NULL;
-            break;
-        }
-        // krbs_tmp has not changed, so if write succeeds,
-        // but sbrk_tmp fails, there are no side-effects
-        if(__STREXW(krbs_tmp, &krbs_ptr))
-            continue;
-        if(__STREXW(sbrk_tmp, &sbrk_ptr))
+        if(__STREXW(sbrk_tmp, (uint32_t *)&sbrk_ptr))
             continue;
         // restore the base pointer of the allocated memory
         sbrk_tmp = sbrk_old;
-    } while( 0 );
-
+        break;
+    }
     return (void *) sbrk_tmp;
 }
 
 void * krbs(const size_t size) {
-    size_t size_internal = size
-    uintptr_t krbs_tmp;
+    uintptr_t krbs_tmp = NULL;
+    size_t size_internal = size;
     // Guarantee minimum allocation size
     if (size_internal < KRBS_INC_MIN) {
-        size_internal = KRBS_INC_MIN
+        size_internal = KRBS_INC_MIN;
+    }
+    size_internal = (size_internal + KRBS_ALIGN - 1) & ~(KRBS_ALIGN - 1);
+
+    while(1) {
+        uintptr_t ptr_diff = __LDREXW((uint32_t *)&sbrk_diff);
+        if (size_internal > ptr_diff) {
+            return NULL;
+        }
+        ptr_diff -= size_internal;
+        if (__STREXW(ptr_diff, (uint32_t *)&sbrk_diff))
+            continue;
+        break;
     }
 
-    do {
-        krbs_tmp = __LDREXW(&krbs_ptr);
-        const uintptr_t sbrk_tmp = __LDREXW(&sbrk_ptr);
-
+    while (1) {
+        krbs_tmp = __LDREXW((uint32_t *)&krbs_ptr);
         // Calculate the new krbs pointer
         krbs_tmp -= size_internal;
         // Guarantee krbs alignment alignment
-        krbs_tmp = krbs_tmp & ~( ((uintptr_t) 1 << KRBS_ALIGN) - 1 );
-
-        if (krbs_tmp < sbrk_tmp) {
-            krbs_tmp = NULL;
-            break;
-        }
-        // sbrk_tmp has not changed, so if write succeeds,
-        // but krbs_tmp fails, there are no side-effects
-        if(__STREXW(sbrk_tmp, &sbrk_ptr))
+        if(__STREXW(krbs_tmp, (uint32_t *)&krbs_ptr))
             continue;
-        if(__STREXW(krbs_tmp, &krbs_ptr))
-            continue;
-    } while( 0 );
-
+        break;
+    }
     return (void *) krbs_tmp;
 }
